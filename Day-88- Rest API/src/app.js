5bb3e7e94e815ec7ec4632332d3fd90048168ebe
Abhay Vishwakarma <gaamaofficial@gmail.com>
2026-09-09 11:56:19 +0530

    [created a server]

--- Day-88- Rest API/src/app.js
+++ Day-88- Rest API/src/app.js
@@ -8,18 +8,40 @@ const arr = [{
     description:''
 }]
 
+// ===================HEALTH==================
+
 app.get('/health', (req, res)=>{
     res.send('Server health is ok')
 })
 
-app.get('/data', (req, res)=>{
+// ===================GETDATA=================
+
+app.get('/getdata', (req, res)=>{
     res.send(arr)
 })
 
-app.post('/data', (req, res)=>{
+// ===================POSTDATA================
+
+app.post('/postdata', (req, res)=>{
     console.log(req.body)
     arr.push(req.body)
     res.send(arr)
+})
+
+// ===================DELETEDATA==============
+
+app.delete('/deletedata/:index', (req, res)=>{
+    console.log(req.params.index)
+    delete arr[req.params.index]
+    res.send('Done')
+})
+
+// ===================UPDATEDATA==============
+
+app.patch('/updatedata/:index', (req, res)=>{
+    arr[req.params.index].description = (req.body.description)
+
+    res.send('description updated successfully')
 })
 
 module.exports= app

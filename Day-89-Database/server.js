293e0d83c0555c4a97a90001d678bab4a0594b51
Abhay Vishwakarma <gaamaofficial@gmail.com>
2026-09-11 22:50:39 +0530

    Database added

--- Day-89-Database/server.js
+++ Day-89-Database/server.js
@@ -1,11 +1,20 @@
 const app = require('./src/app')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const PORT = 3000
 
 app.use(cors())
 
+function ConnectToDB() {
+    mongoose.connect('mongodb+srv://gaamagaming_db_user:xA3lqlcDcnRNNGhN@clustercohort2.ptv6vlu.mongodb.net/Cohort2P0DB')
+    .then(()=>{
+        console.log('Connected to Database')
+    })
+}
+
 
 app.listen(PORT, ()=>{
+    ConnectToDB()
     console.log(`server is running on port no. ${PORT}`)
 })

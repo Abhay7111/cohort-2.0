425add146880f5d971bbdffb43f7de45322c0c0f
Abhay Vishwakarma <gaamaofficial@gmail.com>
2026-09-11 22:18:11 +0530

    server created

--- Day-89-Database/src/app.js
+++ Day-89-Database/src/app.js
@@ -3,10 +3,7 @@ const app = express();
 
 app.use(express.json())
 
-const arr = [{
-    title:'',
-    description:''
-}]
+const arr = []
 
 app.get('/', (req, res)=>{
     res.send(arr)
@@ -22,9 +19,17 @@ app.delete('/delete/:id', (req, res)=>{
     delete arr[req.params.id]
 })
 
-app.patch('/update/:index', (req , res)=>{
+app.patch('/updateTitle/:index', (req , res)=>{
     arr[req.params.index].title = (req.body.title)
     res.send('title updated successfully')
+})
+
+app.patch('/updateDescription/:index', (req, res)=>{
+    arr[req.params.index].description = (req.body.description)
+    res.send('Description is updated successfully')
+    res.status(200).json({
+        message:"description updated"
+    })
 })
 
 module.exports = app

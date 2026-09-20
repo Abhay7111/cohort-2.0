c6670dbd8083e7c71aa2c20bc7341848fd9ccc12
Abhay Vishwakarma <gaamaofficial@gmail.com>
2026-09-20 08:57:06 +0530

    Day 7 Backend Database CRUD Opration Done 👍�

--- Day-91-CRUD-Opration/src/config/database.js
+++ Day-91-CRUD-Opration/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToDB = () => {
-    mongoose.connect('mongodb+srv://gaamagaming_db_user:xA3lqlcDcnRNNGhN@clustercohort2.ptv6vlu.mongodb.net/Cohort-CRUD')
+    mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('DataBase connected successfully')
     })

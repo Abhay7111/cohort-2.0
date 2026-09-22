b43ab0a2d1a7988882fc51fbd255da8d2792e8b4
Abhay Vishwakarma <gaamaofficial@gmail.com>
2026-09-22 09:53:30 +0530

    Created CRUD Opration APIs

--- Day-92-93-FullStack/Backend/Server.js
+++ Day-92-93-FullStack/Backend/Server.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 const app = require('./src/app');
-const cors = require('cors');
 const ConnectToDB = require('./src/Config/Connect');
 
-app.use(cors());
 
 app.listen(process.env.PORT , () => {
     console.log(`Server start successfully on port : ${process.env.PORT}`);
--- Day-92-93-FullStack/Backend/src/app.js
+++ Day-92-93-FullStack/Backend/src/app.js
@@ -1,20 +1,53 @@
 const express = require('express');
 const NoteModel = require('./Models/Notes.Model');
+const cors = require('cors');
 
 const app = express();
-
 app.use(express.json())
 
-app.get('/', async (req, res) => {
+app.use(cors())
+
+app.get('/api/health', (req, res) => {res.send("Server Heath is Ok")})
+
+app.get('/api/note', async (req, res) => {
     const Note = await NoteModel.find()
     res.status(200).json({
-        message:"Data got",
+        message:"Notes fetched successfully",
         Note
     })
 })
 
-app.post('/postnote', (req, res) => {
-    const data = req.body.push()
+app.post('/api/postnote', async (req, res) => {
+    const {title, description} = req.body
+
+    const Note = await NoteModel.create({title, description})
+
+    res.status(201).json({
+        "message" : "Note Created successfully",
+        Note
+    })
+})
+
+app.delete('/api/note/:id', async (req, res) => {
+    const id = req.params.id
+    await NoteModel.findByIdAndDelete(id)
+    console.log(id)
+    res.status(200).json({
+        "message" : "Note deleted successfully",
+        id
+    })
+})
+
+app.patch('/api/note/:id', async (req, res) => {
+    const id = req.params.id
+    const { description } = req.body
+
+    await NoteModel.findByIdAndUpdate(id, { description })
+
+    res.status(200).json({
+        "message":"Note Description updated successfully",
+        description
+    })
 })
 
 module.exports = app
